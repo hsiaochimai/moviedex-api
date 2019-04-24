457351516e6d076038630949e124653e5a4b1afa
Jennifer Hsiao <jchmai21@gmail.com>
2019-04-24 07:54:04 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors=require('cors')
+const helmet= require('helmet')
+
+const MOVIES= require('./movies-data-small.json')
+console.log(process.env.API_TOKEN)
+const app=express()
+    app.use(morgan('dev'))
+    app.use(cors())
+    app.use(helmet())
+ 
+    app.use(function validateBearerToken(req, res, next){
+        const apiToken= process.env.API_TOKEN
+        const authToken= req.get('Authorization')
+        if(!authToken || authToken.split(' ')[1] !== apiToken){
+            return res.status(401).json({error: 'Unauthorized Request'})
+        }
+        next()
+    })
+function handleGetMovies(req, res){
+    if(req.query.genre){
+       results= MOVIES.filter(movie=> movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())) 
+    }
+
+    if(req.query.country){
+        results=MOVIES.filter(movie=> movie.country.toLowerCase().split(" ").join("").includes(req.query.country.toLowerCase().split(" ").join(""))) 
+    }
+
+    if(req.query.avg_vote){
+        results=MOVIES.filter(movie=>{
+            return movie.avg_vote >= req.query.avg_vote
+            })
+    }
+    res.send(results)
+}
+app.get('/movie', handleGetMovies)
+
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
